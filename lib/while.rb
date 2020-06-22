bb5040917a74e330d42008cd796a6665df78937b
Bryn Knowles <bryn.knowles@gmail.com>
2020-06-22 18:51:33 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -5,9 +5,3 @@ while using_while < 10
   puts "Wingardium Leviosa"
   using_while += 1
 end
-
-# counter = 0
-# while counter < 20
-#   puts "The current number is less than 20."
-#   counter += 1
-# end
