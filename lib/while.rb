91cfd6eb92fea0bee5e4685541674baed90a2340
Bryn Knowles <bryn.knowles@gmail.com>
2020-06-22 18:58:59 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -7,11 +7,11 @@
 #   using_while += 1
 # end
 
-# def using_while
-#   using_while = 0
+def using_while
+  using_while = 0
 
     while using_while < 10
       puts "Wingardium Leviosa"
       using_while += 1
     end
-# end
+end
