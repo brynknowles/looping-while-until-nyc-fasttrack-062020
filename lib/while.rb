12f994f19855935d543dfd9dc76804ea1a7abb97
Bryn Knowles <bryn.knowles@gmail.com>
2020-06-22 19:00:54 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -14,3 +14,10 @@
 #     using_while += 1
 #   end
 # end
+def using_while
+  levitation_force = 6
+  #your code here
+  until levitation_force == 10  
+    puts"Wingardium Leviosa"
+    levitation_force += 1
+  end  
